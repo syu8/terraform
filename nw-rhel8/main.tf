bf6b46183a3c8eb6306b7dfc83533fea61d1e20b
Sherry Yu <mhu011@gmail.com>
2020-04-08 16:55:10 -0700

    Update main.tf

--- nw-rhel8/main.tf
+++ nw-rhel8/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "syu-nw8-node1" {
     Name = "syu-nw8-node1"
   }
   availability_zone   = var.us-east-1a
-  subnet_id = var.syu-az-1a-subnetid
+  subnet_id = var.syu-public-subnet
   instance_type = "m5.xlarge"
   root_block_device {
     volume_size = "10"
@@ -26,32 +26,8 @@ resource "aws_instance" "syu-nw8-node2" {
   tags = {
     Name = "syu-nw8-node2"
   }
-  availability_zone   = var.us-east-1c
-  subnet_id = var.syu-az-1c-subnetid
-  instance_type = "m5.xlarge"
-  root_block_device {
-    volume_size = "10"
-  }
-  connection {
-    user = "syu@redhat.com"
-    host = self.public_ip
-  }
-  ami = var.aws_node_ami
-  key_name = var.key_name
-  vpc_security_group_ids = var.public_security_groupids
-  associate_public_ip_address = true
-  disable_api_termination = true
-  ebs_optimized = true
-  source_dest_check = false
-  iam_instance_profile = "sap-Pacemaker"
-}
-
-resource "aws_instance" "syu-nw8-node3" {
-  tags = {
-    Name = "syu-nw8-node3"
-  }
-  availability_zone   = var.us-east-1d
-  subnet_id = var.syu-az-1d-subnetid
+  availability_zone   = var.us-east-1a
+  subnet_id = var.syu-public-subnet
   instance_type = "m5.xlarge"
   root_block_device {
     volume_size = "10"
@@ -74,8 +50,8 @@ resource "aws_instance" "syu-nw8-node4" {
   tags = {
     Name = "syu-nw8-node4"
   }
-  availability_zone   = var.us-east-1c
-  subnet_id = var.syu-az-1c-subnetid
+  availability_zone   = var.us-east-1a
+  subnet_id = var.syu-public-subnet
   instance_type = "m5.xlarge"
   root_block_device {
     volume_size = "10"
@@ -99,7 +75,7 @@ resource "aws_instance" "syu-nw8-node5" {
     Name = "syu-nw8-node5"
   }
   availability_zone   = var.us-east-1a
-  subnet_id = var.syu-az-1a-subnetid
+  subnet_id = var.syu-public-subnet
   instance_type = "m5.xlarge"
   root_block_device {
     volume_size = "10"
@@ -124,7 +100,7 @@ resource "aws_instance" "syu-nw8-hana1" {
     Name = "syu-nw8-hana1"
   }
   availability_zone   = var.us-east-1a
-  subnet_id = var.syu-az-1a-subnetid
+  subnet_id = var.syu-public-subnet
   instance_type = "r4.4xlarge"
   root_block_device {
     volume_size = "120"
